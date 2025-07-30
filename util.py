c63c6096d23cad63db14fd9a0f2e8a0a9ffa96d4
Amir Mustafa <amirengg15@gmail.com>
2025-07-30 17:07:19 +0530

    feat: using prompt in vscode

--- util.py
+++ util.py
@@ -1,2 +1,11 @@
-def convert_f_to_c(fahrenheit):
-    return (fahrenheit - 32) * 5.0 / 9.0
+# Function that takes either a celsius or fahrenheit value
+# value and converts to other units
+
+def convert_temperature(temp, unit):
+    if unit == "C":
+        return (temp - 32) * 5.0 / 9.0
+    elif unit == "F":
+        return (temp * 9.0 / 5.0) + 32
+    else:
+        raise ValueError("Invalid target unit. Please use 'C' or 'F'.")
+    
